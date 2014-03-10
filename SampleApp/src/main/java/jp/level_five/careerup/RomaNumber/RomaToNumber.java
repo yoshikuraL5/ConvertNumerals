d76ce2168e2376236c0cb69c74f9ba4e0970f6cb
TakahikoYoshii <r.yoshii.n@gmail.com>
2014-03-10 15:15:03 +0900

    delete number

--- SampleApp/src/main/java/jp/level_five/careerup/RomaNumber/RomaToNumber.java
+++ SampleApp/src/main/java/jp/level_five/careerup/RomaNumber/RomaToNumber.java
@@ -6,108 +6,109 @@ public class RomaToNumber {
 
 	public int convert(String string) {
 		char[] charArray = new char[20];
-		int number = pushCharArray(string, charArray);
-		for(int i=0; i <= charArray.length - 1; i++){
-			number = valueI(charArray, number, i);
-			number = valueV(charArray, number, i);
-			number = valueX(charArray, number, i);
-			number = valueL(charArray, number, i);
-			number = valueC(charArray, number, i);
-			number = valueD(charArray, number, i);
-			number = valueM(charArray, number, i);
-		 }
+		int number = 0;
+		pushCharArray(string, charArray);
+		for (int i = 0; i <= charArray.length - 1; i++) {
+			number += valueI(charArray, i) + valueV(charArray, i)
+					+ valueX(charArray, i) + valueL(charArray, i)
+					+ valueC(charArray, i) + valueD(charArray, i)
+					+ valueM(charArray, i);
+		}
+		System.out.println(number);
 		checkString(number, string);
 		return number;
 	}
 
-	protected int pushCharArray(String string, char[] charArray) {
+	protected void pushCharArray(String string, char[] charArray) {
 		char[] charArray1 = string.toCharArray();
-		int number = 0;
-		for(int i=0; i <= charArray1.length - 1; i++){
+		for (int i = 0; i <= charArray1.length - 1; i++) {
 			charArray[i] = charArray1[i];
 		}
-		return number;
 	}
 
-	private int valueI(char[] charArray, int number, int i) {
-		if('I' == charArray[i]){
-			number += 1;
-			if(charArray.length > i & ('V' == charArray[i+1] | 'X' == charArray[i+1])){
-				number -= 2;
-			}else if(charArray.length > i+1  & ('V' == charArray[i+2] | 'X' == charArray[i+2])){
-				throw new RuntimeException();
-			}					
-		}
-		return number;
-	}
-	
-
-	protected int valueV(char[] charArray, int number, int i) {
-		if('V' == charArray[i]){
-			number += 5;
-		}
-		return number;
-	}
-
-	private int valueX(char[] charArray, int number, int i) {
-		if('X' == charArray[i]){
-			number += 10;
-			if(charArray.length > i  & ('L' == charArray[i+1] | 'C' == charArray[i+1] )){
-				number -= 20;
-			}else if(charArray.length+1 > i+1 & ('L' == charArray[i+2] | 'C' == charArray[i+2] )){
+	private int valueI(char[] charArray, int i) {
+		if ('I' == charArray[i]) {
+			if (charArray.length > i
+					& ('V' == charArray[i + 1] | 'X' == charArray[i + 1])) {
+				return -1;
+			} else if (charArray.length > i + 1
+					& ('V' == charArray[i + 2] | 'X' == charArray[i + 2])) {
 				throw new RuntimeException();
 			}
+			return 1;
 		}
-		return number;
-	}	
-	
-	protected int valueL(char[] charArray, int number, int i) {
-		if('L' == charArray[i]){
-			number += 50;
-		}
-		return number;
+		return 0;
 	}
 
-	private int valueC(char[] charArray, int number, int i) {
-		if('C' == charArray[i]){
-			number += 100;
-			if(charArray.length > i  & ('D' == charArray[i+1] | 'M' == charArray[i+1])){
-				number -= 200;
-			}else if(charArray.length+1 > i+1  & ('D' == charArray[i+2] | 'M' == charArray[i+2])){
+	protected int valueV(char[] charArray, int i) {
+		if ('V' == charArray[i]) {
+			return 5;
+		}
+		return 0;
+	}
+
+	private int valueX(char[] charArray, int i) {
+		if ('X' == charArray[i]) {
+			if (charArray.length > i
+					& ('L' == charArray[i + 1] | 'C' == charArray[i + 1])) {
+				return -10;
+			} else if (charArray.length + 1 > i + 1
+					& ('L' == charArray[i + 2] | 'C' == charArray[i + 2])) {
 				throw new RuntimeException();
 			}
+			return 10;
 		}
-		return number;
+		return 0;
 	}
 
-	protected int valueD(char[] charArray, int number, int i) {
-		if('D' == charArray[i]){
-			number += 500;
+	protected int valueL(char[] charArray, int i) {
+		if ('L' == charArray[i]) {
+			return 50;
 		}
-		return number;
+		return 0;
 	}
 
-	protected int valueM(char[] charArray, int number, int i) {
-		if('M' == charArray[i]){
-			number += 1000;
+	private int valueC(char[] charArray, int i) {
+		if ('C' == charArray[i]) {
+			if (charArray.length > i
+					& ('D' == charArray[i + 1] | 'M' == charArray[i + 1])) {
+				return -100;
+			} else if (charArray.length + 1 > i + 1
+					& ('D' == charArray[i + 2] | 'M' == charArray[i + 2])) {
+				throw new RuntimeException();
+			}
+			return 100;
 		}
-		return number;
+		return 0;
+	}
+
+	protected int valueD(char[] charArray, int i) {
+		if ('D' == charArray[i]) {
+			return 500;
+		}
+		return 0;
+	}
+
+	protected int valueM(char[] charArray, int i) {
+		if ('M' == charArray[i]) {
+			return 1000;
+		}
+		return 0;
 	}
 
 	private void checkString(int i, String string) {
-		if((i < 1 || 3999 < i) 
-			|| Pattern.matches(".*[IVXLD]M.*", string) 
-			|| Pattern.matches(".*[IVXL]D.*", string)
-			|| Pattern.matches(".*[IVL]C.*", string)
-			|| Pattern.matches(".*[IV]L.*", string)
-			|| Pattern.matches(".*VX.*", string)
-			|| Pattern.matches(".*I{4,}.*", string)
-			|| Pattern.matches(".*V{2,}.*", string)
-			|| Pattern.matches(".*X{4,}.*", string)
-			|| Pattern.matches(".*L{2,}.*", string)
-			|| Pattern.matches(".*C{4,}.*", string)
-			|| Pattern.matches(".*D{2,}.*", string)
-			|| Pattern.matches(".*M{4,}.*", string))
+		if ((i < 1 || 3999 < i) || Pattern.matches(".*[IVXLD]M.*", string)
+				|| Pattern.matches(".*[IVXL]D.*", string)
+				|| Pattern.matches(".*[IVL]C.*", string)
+				|| Pattern.matches(".*[IV]L.*", string)
+				|| Pattern.matches(".*VX.*", string)
+				|| Pattern.matches(".*I{4,}.*", string)
+				|| Pattern.matches(".*V{2,}.*", string)
+				|| Pattern.matches(".*X{4,}.*", string)
+				|| Pattern.matches(".*L{2,}.*", string)
+				|| Pattern.matches(".*C{4,}.*", string)
+				|| Pattern.matches(".*D{2,}.*", string)
+				|| Pattern.matches(".*M{4,}.*", string))
 			throw new RuntimeException();
 	}
 }
--- SampleApp/src/main/java/jp/level_five/careerup/RomaNumber/RomaToNumberExpansion.java
+++ SampleApp/src/main/java/jp/level_five/careerup/RomaNumber/RomaToNumberExpansion.java
@@ -2,92 +2,119 @@ package jp.level_five.careerup.RomaNumber;
 
 import java.util.regex.Pattern;
 
-public class RomaToNumberExpansion extends RomaToNumber{
-	
+public class RomaToNumberExpansion extends RomaToNumber {
+
 	public int convert(String str, String string) {
 		char[] charArray = new char[20];
-		int number = super.pushCharArray(string, charArray);
-		for(int i=0; i <= charArray.length - 1; i++){
-			number = valueIexpand(charArray, number, i);
-			number = valueV(charArray, number, i);
-			number = valueXexpand(charArray, number, i);
-			number = valueL(charArray, number, i);
-			number = valueCexpand(charArray, number, i);
-			number = valueD(charArray, number, i);
-			number = valueM(charArray, number, i);
-		 }
+		int number = 0;
+		super.pushCharArray(string, charArray);
+		for (int i = 0; i <= charArray.length - 1; i++) {
+			number += valueIexpand(charArray, i) + valueV(charArray, i)
+					+ valueXexpand(charArray, i) + valueL(charArray, i)
+					+ valueCexpand(charArray, i) + valueD(charArray, i)
+					+ valueM(charArray, i);
+		}
 		checkStringExpand(number, string);
 		return number;
 	}
-	
-	private int valueIexpand(char[] charArray, int number, int i) {
-		if('I' == charArray[i]){
-			number += 1;
-			if(charArray.length > i & ('V' == charArray[i+1] | 'X' == charArray[i+1] | 'L' == charArray[i+1] | 'C' == charArray[i+1] | 'D' == charArray[i+1] | 'M' == charArray[i+1])){
-				number -= 2;
-			}else if(charArray.length+1 > i & ('V' == charArray[i+2] | 'X' == charArray[i+2] | 'L' == charArray[i+2] | 'C' == charArray[i+2] | 'D' == charArray[i+2] | 'M' == charArray[i+2])){
-				number -= 2;
-			}else if(charArray.length+2 > i & ('V' == charArray[i+3] | 'X' == charArray[i+3] | 'L' == charArray[i+3] | 'C' == charArray[i+3] | 'D' == charArray[i+3] | 'M' == charArray[i+3])){
-				number -= 2;
-			}else if(charArray.length+3 > i & ('V' == charArray[i+4] | 'X' == charArray[i+4] | 'L' == charArray[i+4] | 'C' == charArray[i+4] | 'D' == charArray[i+4] | 'M' == charArray[i+4])){	
-				number -= 2;
-			}else if(charArray.length+4 > i & ('V' == charArray[i+5] | 'X' == charArray[i+5] | 'L' == charArray[i+5] | 'C' == charArray[i+5] | 'D' == charArray[i+5] | 'M' == charArray[i+5])){
+
+	private int valueIexpand(char[] charArray, int i) {
+		if ('I' == charArray[i]) {
+			if (charArray.length > i
+					& ('V' == charArray[i + 1] | 'X' == charArray[i + 1]
+							| 'L' == charArray[i + 1] | 'C' == charArray[i + 1]
+							| 'D' == charArray[i + 1] | 'M' == charArray[i + 1])) {
+				return -1;
+			} else if (charArray.length + 1 > i
+					& ('V' == charArray[i + 2] | 'X' == charArray[i + 2]
+							| 'L' == charArray[i + 2] | 'C' == charArray[i + 2]
+							| 'D' == charArray[i + 2] | 'M' == charArray[i + 2])) {
+				return -1;
+			} else if (charArray.length + 2 > i
+					& ('V' == charArray[i + 3] | 'X' == charArray[i + 3]
+							| 'L' == charArray[i + 3] | 'C' == charArray[i + 3]
+							| 'D' == charArray[i + 3] | 'M' == charArray[i + 3])) {
+				return -1;
+			} else if (charArray.length + 3 > i
+					& ('V' == charArray[i + 4] | 'X' == charArray[i + 4]
+							| 'L' == charArray[i + 4] | 'C' == charArray[i + 4]
+							| 'D' == charArray[i + 4] | 'M' == charArray[i + 4])) {
+				return -1;
+			} else if (charArray.length + 4 > i
+					& ('V' == charArray[i + 5] | 'X' == charArray[i + 5]
+							| 'L' == charArray[i + 5] | 'C' == charArray[i + 5]
+							| 'D' == charArray[i + 5] | 'M' == charArray[i + 5])) {
 				throw new RuntimeException();
-			}					
+			}
+			return 1;
 		}
-		return number;
+		return 0;
 	}
 
-	private int valueXexpand(char[] charArray, int number, int i) {
-		if('X' == charArray[i]){
-			number += 10;
-			if(charArray.length > i  & ('L' == charArray[i+1] | 'C' == charArray[i+1] | 'D' == charArray[i+1] | 'M' == charArray[i+1])){
-				number -= 20;				
-			}else if(charArray.length+1 > i+1 & ('L' == charArray[i+2] | 'C' == charArray[i+2] | 'D' == charArray[i+2] | 'M' == charArray[i+2])){
-				number -= 20;
-			}else if(charArray.length+2 > i+1 & ('L' == charArray[i+3] | 'C' == charArray[i+3] | 'D' == charArray[i+3] | 'M' == charArray[i+3])){
-				number -= 20;
-			}else if(charArray.length+3 > i+1 & ('L' == charArray[i+4] | 'C' == charArray[i+4] | 'D' == charArray[i+4] | 'M' == charArray[i+4])){
-				number -= 20;
-			}else if(charArray.length+4 > i+1 & ('L' == charArray[i+5] | 'C' == charArray[i+5] | 'D' == charArray[i+5] | 'M' == charArray[i+5])){				
+	private int valueXexpand(char[] charArray, int i) {
+		if ('X' == charArray[i]) {
+			if (charArray.length > i
+					& ('L' == charArray[i + 1] | 'C' == charArray[i + 1]
+							| 'D' == charArray[i + 1] | 'M' == charArray[i + 1])) {
+				return -10;
+			} else if (charArray.length + 1 > i + 1
+					& ('L' == charArray[i + 2] | 'C' == charArray[i + 2]
+							| 'D' == charArray[i + 2] | 'M' == charArray[i + 2])) {
+				return -10;
+			} else if (charArray.length + 2 > i + 1
+					& ('L' == charArray[i + 3] | 'C' == charArray[i + 3]
+							| 'D' == charArray[i + 3] | 'M' == charArray[i + 3])) {
+				return -10;
+			} else if (charArray.length + 3 > i + 1
+					& ('L' == charArray[i + 4] | 'C' == charArray[i + 4]
+							| 'D' == charArray[i + 4] | 'M' == charArray[i + 4])) {
+				return -10;
+			} else if (charArray.length + 4 > i + 1
+					& ('L' == charArray[i + 5] | 'C' == charArray[i + 5]
+							| 'D' == charArray[i + 5] | 'M' == charArray[i + 5])) {
 				throw new RuntimeException();
 			}
+			return 10;
 		}
-		return number;
-	}	
+		return 0;
+	}
 
-	private int valueCexpand(char[] charArray, int number, int i) {
-		if('C' == charArray[i]){
-			number += 100;
-			if(charArray.length > i  & ('D' == charArray[i+1] | 'M' == charArray[i+1])){
-				number -= 200;
-			}else if(charArray.length+1 > i+1  & ('D' == charArray[i+2] | 'M' == charArray[i+2])){
-				number -= 200;
-			}else if(charArray.length+2 > i+1  & ('D' == charArray[i+3] | 'M' == charArray[i+3])){
-				number -= 200;
-			}else if(charArray.length+3 > i+1  & ('D' == charArray[i+4] | 'M' == charArray[i+4])){
-				number -= 200;
-			}else if(charArray.length+4 > i+1  & ('D' == charArray[i+5] | 'M' == charArray[i+5])){
+	private int valueCexpand(char[] charArray, int i) {
+		if ('C' == charArray[i]) {
+			if (charArray.length > i
+					& ('D' == charArray[i + 1] | 'M' == charArray[i + 1])) {
+				return -100;
+			} else if (charArray.length + 1 > i + 1
+					& ('D' == charArray[i + 2] | 'M' == charArray[i + 2])) {
+				return -100;
+			} else if (charArray.length + 2 > i + 1
+					& ('D' == charArray[i + 3] | 'M' == charArray[i + 3])) {
+				return -100;
+			} else if (charArray.length + 3 > i + 1
+					& ('D' == charArray[i + 4] | 'M' == charArray[i + 4])) {
+				return -100;
+			} else if (charArray.length + 4 > i + 1
+					& ('D' == charArray[i + 5] | 'M' == charArray[i + 5])) {
 				throw new RuntimeException();
 			}
+			return 100;
 		}
-		return number;
+		return 0;
 	}
 
 	private void checkStringExpand(int i, String string) {
-		if((i < 1 || 3999 < i) 
-			|| Pattern.matches(".*[VLD]M.*", string) 
-			|| Pattern.matches(".*[VL]D.*", string)
-			|| Pattern.matches(".*[VL]C.*", string)
-			|| Pattern.matches(".*[V]L.*", string)
-			|| Pattern.matches(".*VX.*", string)
-			|| Pattern.matches(".*I{5,}.*", string)
-			|| Pattern.matches(".*V{2,}.*", string)
-			|| Pattern.matches(".*X{5,}.*", string)
-			|| Pattern.matches(".*L{2,}.*", string)
-			|| Pattern.matches(".*C{5,}.*", string)
-			|| Pattern.matches(".*D{2,}.*", string)
-			|| Pattern.matches(".*M{4,}.*", string))
+		if ((i < 1 || 3999 < i) || Pattern.matches(".*[VLD]M.*", string)
+				|| Pattern.matches(".*[VL]D.*", string)
+				|| Pattern.matches(".*[VL]C.*", string)
+				|| Pattern.matches(".*[V]L.*", string)
+				|| Pattern.matches(".*VX.*", string)
+				|| Pattern.matches(".*I{5,}.*", string)
+				|| Pattern.matches(".*V{2,}.*", string)
+				|| Pattern.matches(".*X{5,}.*", string)
+				|| Pattern.matches(".*L{2,}.*", string)
+				|| Pattern.matches(".*C{5,}.*", string)
+				|| Pattern.matches(".*D{2,}.*", string)
+				|| Pattern.matches(".*M{4,}.*", string))
 			throw new RuntimeException();
 	}
 }
